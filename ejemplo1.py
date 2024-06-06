f2e38fe150699170d3d4aef8be18ae3c345352f1
Cris-502 <2023-40191@liceocanadiense.edu.gt>
2024-06-06 14:27:37 -0600

    se van a subir tres proyectos

---
+++ ejemplo1.py
@@ -0,0 +1,14 @@
+print("Ingrese 5 numeros")
+a=3
+b=7
+c=5
+d=6
+e=4
+suma=a+b+c+d+e
+resta=a-b-c-d-e
+multi=a*b*c*d*e
+divi=a/b/c/d/e
+print("la suma de los numeros es:",suma)
+print("la resta de los numeros es:",resta)
+print("la multiplicacion de los numeros es:",multi)
+print("la division de los numeros es:",divi)
